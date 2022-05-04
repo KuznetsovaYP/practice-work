0d054979faa15ef73ce7d1efe1a529025fd21dbc
Your Name <you@example.com>
2022-05-04 15:33:58 +0300

    2

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 
-void enter(int n, int k, vector<double>&A)
+void enter( int k, vector<double>&A)
 {
-    for (int i=n; i<=k; i++)
+    for (int i=0; i<k; i++)
         cin>>A[i];
     return;
 }
@@ -18,38 +18,38 @@ void enter(int n, int k, vector<double>&A)
 void t1(int N, int k, vector<double>&a, vector<vector<double>>&X,vector<double>&T, vector<double>&M)
 {
     vector<double> s(k);
-    s[1]=0;
-    for (int i=1; i<=k; i++)
+    s[0]=0;
+    for (int i=0; i<k; i++)
     {
-        for (int j=1; j<=N; j++)
+        for (int j=0; j<N; j++)
             s[i]=s[i]+X[i][j];
     }
-    for(int i=1; i<=k; i++)
+    for(int i=0; i<k; i++)
          {
-        for (int j=1; j<=N; j++)
+        for (int j=0; j<N; j++)
         {
             T[i] = a[i]*s[i]/N;
-            cout<<M[i]<<' '<<X[i][j]<<' '<<T[i]<<'\t';
+            cout<<"chanel number: "<<M[i]<<" mesurement: "<<X[i][j]<<"  TEP1: "<<T[i]<<endl;
         }
          }
     return;
 }
 
-void t2(int k, int N, vector<double>&b, vector<double>&g, vector<vector<double>>&X,vector<double>&T, vector<double>&M)
+void t2(int N, int k, vector<double>&b, vector<double>&g, vector<vector<double>>&X,vector<double>&T, vector<double>&M)
 {
     vector<double> c(k);
-    c[1]=0;
-    for (int i=1; i<=k; i++)
+    c[0]=0;
+    for (int i=0; i<k; i++)
     {
-        for (int j=1; j<=N; j++)
+        for (int j=0; j<N; j++)
             c[i]=c[i]+pow((X[i][j]-g[i]),2);
     }
-    for(int i=1; i<=k; i++)
+    for(int i=0; i<k; i++)
          {
-        for (int j=1; j<=N; j++)
+        for (int j=0; j<N; j++)
     {
         T[i] = b[i]*c[i]/N;
-        cout<<M[i]<<' '<<X[i][j]<<' '<<T[i]<<'\t';
+        cout<<"chanel number: "<<M[i]<<" mesurement: "<<X[i][j]<<"  TEP2: "<<T[i]<<endl;
     }
          }
     return;
@@ -73,30 +73,29 @@ int main()
     vector<vector<double>>X(k,vector<double>(N));
 
     cout<<"Enter the survey channel numbers: ";
-    enter(1,k,M);
+    enter(k,M);
 
     cout<<"Enter constants ai: ";
-    enter(1,k,a);
+    enter(k,a);
 
     cout<<"Enter constants bi: ";
-    enter(1,k,b);
+    enter(k,b);
 
     cout<<"Enter constants gi: ";
-    enter(1,k,g);
+    enter(k,g);
 
     vector<double>TEP1(k);
     vector<double>TEP2(k);
 
-    for(int j=1; j<=N; j++)
+    for(int j=0; j<N; j++)
     {
-        for (int i=1; i<=k; i++)
+        for (int i=0; i<k; i++)
             X[i][j]=plant_measure(M[i], plant);
     }
 
     t1(N,k,a,X,TEP1,M);
     t2(N,k,b,g,X,TEP2,M);
 
-    //таблица
 
     return 0;
 }
